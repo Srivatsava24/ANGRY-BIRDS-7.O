f656f57c3b0d660d9a27eef18fe5d00438a80117
Srivatsava24 <73235587+Srivatsava24@users.noreply.github.com>
2020-11-26 18:51:16 +0530

    Add files via upload

---
+++ Pig.js
@@ -0,0 +1,22 @@
+class Pig extends BaseClass {
+  constructor(x, y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/enemy.png");
+    this.Visiblity = 125
+  }
+ display(){
+   if (this.body.speed<3){
+   super.display()
+   console.log(this.body.speed)
+   }
+   else{
+     push()
+     World.remove(world,this.body)
+     this.Visiblity = this.Visiblity-5
+     tint(230,0,252,this.Visiblity)
+     image(this.image, this.body.position.x,this.body.position.y,50,50)
+     pop()
+   }
+ }
+ 
+};
